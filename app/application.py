3636af2a612e22a045eb54b6808583cd4901c757
Brett <brettllapierre@gmail.com>
2023-03-26 11:21:59 -0700

    Added initial unit tests, and added outside snip color picker
    Added initial unit tests, and added outside snip color picker

--- app/application.py
+++ app/application.py
@@ -1,29 +1,57 @@
 from tkinter import *
+from tkinter import colorchooser
 from screenshot_tools import SnipRectangle, bounded_screenshot
 
 
 class Application:
     def __init__(self, master: Tk) -> None:
         self.master = master
-        self.rect = SnipRectangle()
 
         self.master.geometry("400x50+200+200")
         self.master.title("Lil Snippy")
 
+        self.add_menu_bar()
         self.menu_frame = Frame(master)
         self.menu_frame.pack(fill=BOTH, expand=YES, padx=1, pady=1)
+        self.add_menu_buttons()
 
-        self.buttonBar = Frame(self.menu_frame)
-        self.buttonBar.pack()
+        self.outside_snip_color = ["#a0a0a0"]
+
+    def add_menu_buttons(self) -> None:
+        self.menu_button_bar = Frame(self.menu_frame)
+        self.menu_button_bar.pack()
 
         self.snipButton = Button(
-            self.buttonBar,
+            self.menu_button_bar,
             width=5,
             height=5,
             command=self.create_snipping_environment,
             background="green",
         )
         self.snipButton.pack()
+
+    def add_menu_bar(self) -> None:
+        self.menu_bar = Menu(self.master)
+        self.master.config(menu=self.menu_bar)
+
+        file_menu = Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Quit", command=self.master.quit)
+
+        options_menu = Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Customize", menu=options_menu)
+        options_menu.add_command(label="Colors", command=self.open_color_window)
+
+        about_menu = Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="About", menu=about_menu)
+        about_menu.add_command(label="Info", command=self.master.quit)
+
+    def open_color_window(self):
+        self.outside_snip_color = colorchooser.askcolor()
+
+    def close_slider_window(self, slider_window):
+        slider_window.destroy()
+        self.slider_window_open = False
 
     def create_snipping_environment(self) -> None:
         self.master.withdraw()
@@ -40,7 +68,9 @@ class Application:
         self.snip_frame.pack(fill=BOTH, expand=YES)
 
         # Create snipping surface (everything outside of the snip square)
-        self.snip_surface = Canvas(self.snip_frame, cursor="cross", bg="grey11")
+        self.snip_surface = Canvas(
+            self.snip_frame, cursor="cross", bg=self.outside_snip_color[-1]
+        )
         self.snip_surface.pack(fill=BOTH, expand=YES)
         # Create snipping coordinates
         self.coords_label_window = Toplevel()
@@ -58,7 +88,7 @@ class Application:
 
     def on_snip_button_press(self, event: Event) -> None:
         """Saves the inital position of the rectangle on the screan"""
-        self.rect.x1, self.rect.y1 = event.x, event.y
+        self.rect = SnipRectangle(x1=event.x, y1=event.y)
         self.snip_surface.create_rectangle(
             0, 0, 1, 1, outline="red", width=3, fill="maroon3"
         )
---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from app.screenshot_tools import SnipRectangle
+
+
+@pytest.fixture
+def rectangle():
+    return SnipRectangle
